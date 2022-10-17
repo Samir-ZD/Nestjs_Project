48b393b0f771412750fe918ae511ae787cc92614
Samir-zd <samir.zahreddine@outlook.com>
2022-10-18 01:58:36 +0300

    Creating Project, Create Database and User Module

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: '',
+      host: '',
+      port: ,
+      username: '',
+      password: '',
+      database: '',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
